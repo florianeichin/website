06b3b58e80fbfeb0ec3a4e44be9ecf98df3a3794
Florian Eichin <flo.ei@web.de>
2019-02-11 14:45:26 +0100

    minor fix

--- func.js
+++ func.js
@@ -1,35 +1,34 @@
-function useIt(page) {
-    //var link = document.querySelector('link[rel="import"]');
-    //var content = link.import;
-    //var el = content.querySelector('.about');
-    var el = document.querySelector("template").content.querySelector('.' + page)
-    console.log(el)
+function navigate(page) {
+  var el = document.querySelector("template").content.querySelector('.' + page)
 
-    if (document.querySelector("#container").hasChildNodes()) {
-      document.querySelector("#container").lastChild.remove()
-    }
-
-    document.querySelector('#container').appendChild(
-      document.importNode(el, true));
-
-    myFunction()
+  if (document.querySelector("#container").hasChildNodes()) {
+    document.querySelector("#container").lastChild.remove()
   }
 
-  function myFunction() {
-    var x = document.getElementById("menu");
-    if (x.className === "navigation") {
-      x.className += " responsive";
-    } else {
-      x.className = "navigation";
-    }
-    var x = document.getElementById("menu-icon");
-    if (x.className === "fa fa-bars") {
-      x.className = "fa fa-times";
-    } else {
-      x.className = "fa fa-bars";
-    }
+  document.querySelector('#container').appendChild(
+    document.importNode(el, true));
 
+
+}
+
+function menuToggle() {
+  var x = document.getElementById("menu");
+  if (x.className === "navigation") {
+    x.className += " responsive";
+  } else {
+    x.className = "navigation";
   }
+  menuIconToggle()
+}
+
+function menuIconToggle(){
+  var x = document.getElementById("menu-icon");
+  if (x.className === "fa fa-bars") {
+    x.className = "fa fa-times";
+  } else {
+    x.className = "fa fa-bars";
+  }
+}
 
 
-  useIt("about")
+navigate("about")
